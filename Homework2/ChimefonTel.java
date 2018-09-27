e97554b0fb61c7a63f66360117fdcb7d9c01a80d
Ari-98 <i-samar98@hotmail.es>
2018-09-27 09:16:09 -0500

    27 september

--- Homework2/ChimefonTel.java
+++ Homework2/ChimefonTel.java
@@ -1,43 +1,40 @@
 import java.util.Scanner;
-  public class ChimefonTel{
-    public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
+public class ChimefonTel{
+ public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   Double pag;
+   Double imp;
+   Double tot;
+   pag = 0.0;
+   imp = 0.0;
 
-      System.out.println("Tipo de dia (dia habil o domingo):");
-      String di = input.nextLine();
-      System.out.println("Turno matutino(M) o vespertino (V):");
-      String tu = input.nextLine();
-      double imp, tot;
-
-
-      System.out.println("Tiempo:");
-      int ti = input.nextInt();
-      if (ti <= 5) {
-        double pag = ti *1;
-      if (ti <= 8) {
-        pag = (ti -5)*.08+5.0;
-      if (ti <= 10) {
-        pag = (ti -8)*.07+7.4;
-      }else{
-        pag = (ti -10)*.05+8.8;
-      }
-}
-    System.out.println("El pago es de: $"+pag);
-    if (di.equalsIgnoreCase ("domingo")) {
-      imp= pag *0.03;
-      if (di.equalsIgnoreCase ("habil")){
-         if (tu.equalsIgnoreCase ("M")) {
-        imp = pag * 0.15;
-      }
-      if (tu.equalsIgnoreCase ("V")) {
-     imp = pag * 0.10;
+   System.out.println("Enter the minutes of the call");
+   int ti = input.nextInt();
+   if (ti <= 5) {
+     pag = ti * 1.0;
+   } else if (ti > 5 && ti <= 8)  {
+     pag = ((ti - 5) * .80) + 5;
+   } else if (ti > 8 && ti <= 10) {
+     pag = ((ti - 8) * .70) + 7.4;
+   } else if (ti > 10) {
+     pag = ((ti - 8) * .70) + 7.4;
    }
+   System.out.println("Enter the day of the call. Example: domingo or dom");
+   String di = input.next();
+  if (di.equalsIgnoreCase ("domingo") || di.equalsIgnoreCase ("dom")) {
+    imp = (pag * .03) ;
+  } else {
+    System.out.println("Enter the day shift: matutino or vespertino");
+    String tu = input.next();
+    if (tu.equalsIgnoreCase ("matutino")) {
+      imp = (pag * .15) ;
+    } else if (tu.equalsIgnoreCase ("vespertino")) {
+      imp = (pag * .10);
     }
-    System.out.println("El impuesto es de: $"+imp);
-    tot= pag+imp;
-    System.out.println("El pago total es de: $"+tot);
-      }
-
   }
-      }
-    }
+  tot = imp + pag;
+  System.out.println("The payment is: $" + pag);
+  System.out.println("The tax is: $" + imp);
+  System.out.println("The total payment is: $" + tot);
+}
+}
